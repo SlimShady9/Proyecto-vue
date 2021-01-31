2b2bc27600e627a6b280b75b6e8af5dcf0617cad
Juan David Alberto Quintero <slim.shady99q@gmail.com>
2021-01-31 09:45:33 -0500

    commit inicial

---
+++ src/components/Lista.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(fruta, index) in arrayOrdena"
+          :key="fruta.id">
+              {{fruta.nombre}}
+              <button @click="aumentar(index)">{{fruta.cantidad}}</button>
+          </li>
+          <button class="reiniciar" @click="reiniciar">Reiniciar</button>
+      </ul>
+  </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+
+    computed:{
+        ...mapState(['frutas']),
+        arrayOrdena(){
+             let frutas = this.frutas;
+            return frutas.sort((a, b) => b.cantidad - a.cantidad);
+
+        }
+    },
+
+    methods:{
+        ...mapMutations(['aumentar', 'reiniciar'])
+    }
+
+}
+</script>
+
+<style>
+
+ul{
+    list-style: none;
+    padding: 0;
+    
+}
+
+li{
+    height: 2em;
+    border-radius: 5px;
+    margin: .75em 0 0 0;
+    background-color: rgb(98, 155, 98);
+    color: white;
+    padding-top: 15px;
+    padding-left: 25px;
+    text-align: left;
+    
+}
+li > button{
+    background-color: rgb(60, 60, 223);
+    color: white;
+    float: right;
+    margin-right: 10px;
+    padding-right: .7em;
+    border: transparent;
+    border-radius: 25px
+}
+
+li > button:active{
+    background-color: rgb(157, 157, 211);
+}
+
+.reiniciar{
+    margin-top: 1em;
+    width: 100%;
+    background-color: rgb(212, 63, 63);
+    border: transparent;
+    color: rgb(255, 255, 255);
+    border-radius: 5px;
+    font-size: 1.45em;
+    padding: 10px 0 10px 0;
+}
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas:[{nombre:'Manzana', cantidad:0},{nombre:'Pera', cantidad:2},{nombre:'Naranja', cantidad:3}]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++
+    },
+    reiniciar(state){
+      state.frutas.forEach(x => x.cantidad = 0)
+    }
   },
+  
   actions: {
   },
   modules: {
